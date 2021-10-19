3d801d504dfd4eac60d46329f3df9ce7a9bc8edc
EhWanu <cameronjshadgett@gmail.com>
2021-10-19 16:07:17 -0400

    implemented sidebar with logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/about/About";
 import Landing from "./components/landing/Landing";
 import Navbar from "./components/navbar/Navbar";
 import { useState } from "react";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
 	const [menuOpen, setMenuOpen] = useState(false);
@@ -11,10 +12,10 @@ function App() {
 				menuOpen={menuOpen}
 				setMenuOpen={setMenuOpen}
 			/>
-			{/* <Sidebar
+			<Sidebar
 				menuOpen={menuOpen}
 				setMenuOpen={setMenuOpen}
-			/> */}
+			/>
 			<Landing />
 			<About />
 		</div>
