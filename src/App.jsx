2f857172870d73f91b5ec8161c6c6c159c24409b
EhWanu <cameronjshadgett@gmail.com>
2021-10-19 15:53:58 -0400

    setup useState for navbar animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import About from "./components/about/About";
 import Landing from "./components/landing/Landing";
+import Navbar from "./components/navbar/Navbar";
+import { useState } from "react";
 
 function App() {
+	const [menuOpen, setMenuOpen] = useState(false);
 	return (
 		<div className="app">
+			<Navbar
+				menuOpen={menuOpen}
+				setMenuOpen={setMenuOpen}
+			/>
+			{/* <Sidebar
+				menuOpen={menuOpen}
+				setMenuOpen={setMenuOpen}
+			/> */}
 			<Landing />
 			<About />
 		</div>
