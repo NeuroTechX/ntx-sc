329778c390934bebe7805786afafb97f79368083
EhWanu <cameronjshadgett@gmail.com>
2021-10-22 11:24:01 -0400

    added bar as separate component

--- src/components/WhyJoinBar/WhyJoinBar.jsx
+++ src/components/WhyJoinBar/WhyJoinBar.jsx
@@ -1,7 +1,14 @@
-import "./WhyJoinBar.scss";
+import "./whyJoinBar.scss";
 
 const WhyJoinBar = () => {
-	return <div className="WhyJoinBar"></div>;
+	return (
+		<div className="WhyJoinBar">
+			<div className="WhyJoinText">
+				WHY JOIN THE NEUROTECHX
+				<br /> STUDENT CLUB INITIATIVE?
+			</div>
+		</div>
+	);
 };
 
 export default WhyJoinBar;
