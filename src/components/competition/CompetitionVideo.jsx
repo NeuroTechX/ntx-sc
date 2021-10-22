5334420ce06715a5830da89423dfbccfb07e07c7
EhWanu <cameronjshadgett@gmail.com>
2021-10-22 15:13:10 -0400

    fixed percentages, temporary background video player box

--- src/components/competition/CompetitionVideo.jsx
+++ src/components/competition/CompetitionVideo.jsx
@@ -1,15 +1,9 @@
 import "./competition.scss";
-import Caligraphy from "../img/light blue 1.png";
-import Background from "../img/neurotechx1-bckgnd.png";
+import Caligraphy from "../img/light blue.png";
 
 const Competition = () => {
 	return (
-		<div
-			className="competition"
-			style={{
-				backgroundImage: `url(${Background})`,
-			}}
-		>
+		<div className="competition">
 			<div className="competition-wrapper">
 				<div className="archway">
 					<div className="a-top">
