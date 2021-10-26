87f5c7d7a47a0ff06433a223fc05907351d6b1ae
EhWanu <cameronjshadgett@gmail.com>
2021-10-26 15:56:29 -0400

    added logos and hover effects to partners section

--- src/components/partner/Partners.jsx
+++ src/components/partner/Partners.jsx
@@ -6,18 +6,51 @@ const Partners = () => {
 			<div className="container">
 				<div className="top">
 					<div className="topCard">
-						<img src="" alt="" />
+						<img
+							src="https://d3n8a8pro7vhmx.cloudfront.net/branchout/sites/1/meta_images/original/branchout-logo.png?1422309089"
+							alt=""
+						/>
 					</div>
 				</div>
 				<div className="center">
-					<div className="centerCard"></div>
-					<div className="centerCard"></div>
-					<div className="centerCard"></div>
-					<div className="centerCard"></div>
+					<div className="centerCard">
+						<img
+							src="https://www.lulzbot.com/sites/default/files/openbci-logo.png"
+							alt=""
+						/>
+					</div>
+					<div className="centerCard">
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Neuralink_logo.svg/1200px-Neuralink_logo.svg.png"
+							alt=""
+						/>
+					</div>
+					<div className="centerCard">
+						<img
+							src="https://images.squarespace-cdn.com/content/v1/5a0c82936957da9f556f5c30/1546991933340-F39NG3RW6TKBTS6506Y4/CXN_WMARK_FC_DARK_DGTL.png?format=2500w"
+							alt=""
+						/>
+					</div>
+					<div className="centerCard">
+						<img
+							src="https://maat.neurable.com/uploads/-/system/appearance/logo/1/LogoDark_StackCMYK.png"
+							alt=""
+						/>
+					</div>
 				</div>
 				<div className="bottom">
-					<div className="bottomCard"></div>
-					<div className="bottomCard"></div>
+					<div className="bottomCard">
+						<img
+							src="https://www.gtec.at/wp-content/themes/gtc/images/logo.svg"
+							alt=""
+						/>
+					</div>
+					<div className="bottomCard">
+						<img
+							src="https://brainvision.com/wp-content/uploads/2016/11/Logo_BrainProducts.png"
+							alt=""
+						/>
+					</div>
 					<div className="bottomCard"></div>
 					<div className="bottomCard"></div>
 					<div className="bottomCard"></div>
