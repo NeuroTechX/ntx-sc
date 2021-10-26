8142c09e0283c576f1dda05c874e0e6348d8130d
EhWanu <cameronjshadgett@gmail.com>
2021-10-25 22:29:49 -0400

    centred info boxes over images and added animations

--- src/components/JoinInfo/JoinInfo.jsx
+++ src/components/JoinInfo/JoinInfo.jsx
@@ -4,21 +4,58 @@ const JoinInfo = () => {
 	return (
 		<div className="joinInfo">
 			<div className="container">
+				<div className="infoContainer">
+					<div className="infoBox">
+						<h1>
+							I AM A STUDENT INTERESTED IN STARTING A
+							CLUB
+						</h1>
+					</div>
+					<div className="infoBox">
+						<h1>
+							I AM A COMPANY INTERESTED IN PARTNERING
+						</h1>
+					</div>
+					<div className="infoBox">
+						<h1>
+							I AM A STUDENT WITH AN NTX AFFILIATED CLUB
+							LOOKING FOR RESOURCES
+						</h1>
+					</div>
+					<div className="infoBox">
+						<h1>
+							I’M CURIOUS ABOUT THE PROJECTS STUDENTS ARE
+							WORKING ON
+						</h1>
+					</div>
+				</div>
 				<div className="cards">
 					<div className="card">
-						<img src="" alt="" />
+						<img
+							src="https://images.unsplash.com/photo-1550071593-fd1bdaf1f93c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+							alt=""
+						/>
 						<h2></h2>
 					</div>
 					<div className="card">
-						<img src="" alt="" />
+						<img
+							src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1484&q=80"
+							alt=""
+						/>
 						<h2></h2>
 					</div>
 					<div className="card">
-						<img src="" alt="" />
+						<img
+							src="https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+							alt=""
+						/>
 						<h2></h2>
 					</div>
 					<div className="card">
-						<img src="" alt="" />
+						<img
+							src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80 "
+							alt=""
+						/>
 						<h2></h2>
 					</div>
 				</div>
