d80b097d9ca683b53f5307833a257cdcc6f13dbb
EhWanu <cameronjshadgett@gmail.com>
2021-11-04 16:14:23 -0400

    added previous supporters page

--- src/pages/partnersPage/PartnersPage.jsx
+++ src/pages/partnersPage/PartnersPage.jsx
@@ -1,14 +1,8 @@
 import Main from "./components/main partners/Main";
 import Landing from "./components/partnersLanding/PartnersLanding";
-import WhyJoinBar from "./components/whyJoin/WhyJoinBar";
-import WhyJoinInfo from "./components/whyJoin/WhyJoinInfo";
-import JoinInfoBar from "./components/JoinInfo/JoinInfoBar";
-import JoinInfo from "./components/JoinInfo/JoinInfo";
-import News from "./components/news/News";
 import Partners from "./components/influencer/Influencers";
-import FAQ from "./components/FAQ/FAQ";
-import Dashboard from "./components/dashboard/Dashboard";
 import Supporters from "./components/supporter/Supporters";
+import PreviousSupporters from "./components/previous supporters/PreviousSupporters";
 
 function Home() {
 	return (
@@ -17,14 +11,7 @@ function Home() {
 			<Main />
 			<Partners />
 			<Supporters />
-			<WhyJoinBar />
-			<WhyJoinInfo />
-			<JoinInfoBar />
-			<JoinInfo />
-			<News />
-
-			<FAQ />
-			<Dashboard />
+			<PreviousSupporters />
 		</div>
 	);
 }
