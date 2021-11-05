bccd38803cb5dfee77b6f65442ef16912ac58465
EhWanu <cameronjshadgett@gmail.com>
2021-11-05 05:54:22 -0400

    added fields domains page

--- src/pages/get started page/GetStarted.jsx
+++ src/pages/get started page/GetStarted.jsx
@@ -2,6 +2,8 @@ import GtLanding from "./components/GsLanding/GsLanding";
 import Gs1 from "./components/gs1/Gs1";
 import Gs2 from "./components/gs2/Gs2";
 import Implications from "./implications/Implications";
+import Activities from "./components/activities/Activities";
+import Fields from "./components/fields/domains/Fields";
 
 function GetStarted() {
 	return (
@@ -10,6 +12,8 @@ function GetStarted() {
 			<Gs2 />
 			<Gs1 />
 			<Implications />
+			<Activities />
+			<Fields />
 		</div>
 	);
 }
---
+++ src/pages/get started page/components/fields/domains/Fields.jsx
@@ -0,0 +1,52 @@
+import "./fields.scss";
+
+export default function Fields() {
+	return (
+		<div className="fields">
+			<div className="banner">
+				<h1>FIELDS/DOMAINS</h1>
+			</div>
+			<div className="container">
+				<div className="textBox">
+					Here is a non-exhaustive list of fields that
+					could be explored through a NeuroTech Student
+					Club. Click here for details on
+					neurotechnology applications.
+					<br />
+					<br />
+					<ul>
+						<li>Neurotechnology</li>
+						<li>Virtual Reality</li>
+						<li>Machine Learning</li>
+						<li>Signal Processing</li>
+						<li>Electronics / Hardware</li>
+						<li>3D Printing</li>
+						<li>Big Data</li>
+						<li>Data Science</li>
+						<li>Biosignals</li>
+						<li>Wearables</li>
+						<li>IoT</li>
+						<li>Psychology</li>
+						<li>Cognitive Science</li>
+						<li>Computational Neuroscience</li>
+						<br />
+						<br />
+						NeuroTechnology is a very multidisciplinary
+						field. We encourage the student clubs to
+						explore the fields they are interested in
+						throughout the year and present their
+						project for the annual competition in the
+						“Open Challenge” category. Check the
+						competition document for more information.
+					</ul>
+				</div>
+			</div>
+			<div className="imgContainer">
+				<img
+					src="https://images.unsplash.com/photo-1507537417841-81e85feb9bd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
+					alt=""
+				/>
+			</div>
+		</div>
+	);
+}
