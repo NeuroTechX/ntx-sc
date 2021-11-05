f994b2cd938165cfd075bebcdbbacb580a898b61
EhWanu <cameronjshadgett@gmail.com>
2021-11-05 04:27:18 -0400

    implications and relationships section

--- src/pages/get started page/GetStarted.jsx
+++ src/pages/get started page/GetStarted.jsx
@@ -1,6 +1,7 @@
 import GtLanding from "./components/GsLanding/GsLanding";
 import Gs1 from "./components/gs1/Gs1";
 import Gs2 from "./components/gs2/Gs2";
+import Implications from "./implications/Implications";
 
 function GetStarted() {
 	return (
@@ -8,6 +9,7 @@ function GetStarted() {
 			<GtLanding />
 			<Gs2 />
 			<Gs1 />
+			<Implications />
 		</div>
 	);
 }
