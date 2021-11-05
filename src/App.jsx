148378b59adea735cbf29a289d141257d128ab72
EhWanu <cameronjshadgett@gmail.com>
2021-11-05 07:58:10 -0400

    hide errors for hosting

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
-	Link,
 } from "react-router-dom";
 import Sidebar from "./components/sidebar/Sidebar";
 import Footer from "./components/footer/Footer";
