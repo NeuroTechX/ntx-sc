f3ebe83478e38eb8679d101d5863741248e3b68f
EhWanu <cameronjshadgett@gmail.com>
2021-11-09 11:14:57 -0500

    added social links to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,54 @@
 import "./footer.scss";
+import logo from "./icons/Logo.png";
 
-import { Twitter } from "@mui/icons-material";
+import {
+	Twitter,
+	Facebook,
+	LinkedIn,
+	GitHub,
+} from "@mui/icons-material";
 
 const Footer = () => {
 	return (
 		<div className="footer">
 			<div className="footerContainer">
 				<div className="left">
-					<div className="textBox"></div>
+					<a
+						href="https://neurotechx.com/"
+						target="blank"
+					>
+						<img
+							src={logo}
+							alt="Logo"
+							className="icon"
+						/>
+					</a>
 				</div>
 				<div className="right">
-					<Twitter />
+					<a
+						href="https://twitter.com/neurotechx"
+						target="blank"
+					>
+						<Twitter className="icon" />
+					</a>
+					<a
+						href="https://www.facebook.com/neurotechx"
+						target="blank"
+					>
+						<Facebook className="icon" />
+					</a>
+					<a
+						href="https://www.linkedin.com/company/neurotechx/"
+						target="blank"
+					>
+						<LinkedIn className="icon" />
+					</a>
+					<a
+						href="https://github.com/NeuroTechX/"
+						target="blank"
+					>
+						<GitHub className="icon" />
+					</a>
 				</div>
 			</div>
 		</div>
