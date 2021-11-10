87a3347eee39266cfb8fa5640c95a942628d9432
EhWanu <cameronjshadgett@gmail.com>
2021-11-10 10:23:51 -0500

    added images and hover to dashboard

--- src/pages/home/components/dashboard/Dashboard.jsx
+++ src/pages/home/components/dashboard/Dashboard.jsx
@@ -6,26 +6,97 @@ const Dashboard = () => {
 			<div className="dashboard">
 				<div className="container">
 					<div className="cards">
-						{/* <h1 className="title">fdsa</h1> */}
-						<div className="card"></div>
-						{/* <h1 className="title">fdsa</h1> */}
-						<div className="card"></div>
-						{/* <h1 className="title">fdsa</h1> */}
-						<div className="card"></div>
-						{/* <h1 className="title">fds</h1> */}
-						<div className="card"></div>
-						{/* <h1 className="title">fdsa</h1> */}
-						<div className="card"></div>
-						{/* <h1 className="title">fdsa</h1> */}
-						<div className="card"></div>
-						{/* <h1 className="title">fdsa</h1> */}
-						<div className="card"></div>
-						{/* <h1 className="title">fdsa</h1> */}
-						<div className="card"></div>
-						{/* <h1 className="title">fdsa</h1> */}
-						<div className="card"></div>
-						{/* <h1 className="title">fdsa</h1> */}
-						<div className="card"></div>
+						<div className="card">
+							<img
+								src="https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+								alt=""
+							/>
+							<div className="textBox">
+								<div className="text">EVENTS</div>
+							</div>
+						</div>
+
+						<div className="card">
+							<img
+								src="https://images.unsplash.com/photo-1600880292089-90a7e086ee0c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
+								alt=""
+							/>
+							<div className="textBox">
+								<div className="text">PARTNERS</div>
+							</div>
+						</div>
+						<div className="card">
+							<img
+								src="https://images.unsplash.com/photo-1634813052299-35818e3fb131?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+								alt=""
+							/>
+							<div className="textBox">
+								<div className="text">CLUBS</div>
+							</div>
+						</div>
+						<div className="card">
+							<img
+								src="https://images.unsplash.com/photo-1518481612222-68bbe828ecd1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+								alt=""
+							/>
+							<div className="textBox">
+								<div className="text">ABOUT</div>
+							</div>
+						</div>
+						<div className="card">
+							<img
+								src="https://images.unsplash.com/photo-1604948501466-4e9c339b9c24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+								alt=""
+							/>
+							<div className="textBox">
+								<div className="text">COMPETITION</div>
+							</div>
+						</div>
+						<div className="card">
+							<img
+								src="https://images.unsplash.com/photo-1518152006812-edab29b069ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+								alt=""
+							/>
+							<div className="textBox">
+								<div className="text">PROJECTS</div>
+							</div>
+						</div>
+						<div className="card">
+							<img
+								src="https://images.unsplash.com/photo-1598618589821-f031d29a366f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+								alt=""
+							/>
+							<div className="textBox">
+								<div className="text">RESOURCES</div>
+							</div>
+						</div>
+						<div className="card">
+							<img
+								src="https://images.unsplash.com/photo-1557318041-1ce374d55ebf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80"
+								alt=""
+							/>
+							<div className="textBox">
+								<div className="text">FAQS</div>
+							</div>
+						</div>
+						<div className="card">
+							<img
+								src="https://images.unsplash.com/photo-1461896836934-ffe607ba8211?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+								alt=""
+							/>
+							<div className="textBox">
+								<div className="text">GET STARTED</div>
+							</div>
+						</div>
+						<div className="card">
+							<img
+								src="https://images.unsplash.com/photo-1585829365295-ab7cd400c167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+								alt=""
+							/>
+							<div className="textBox">
+								<div className="text">NEWS</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
