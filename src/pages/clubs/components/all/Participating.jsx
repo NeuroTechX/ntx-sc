4aea847ebd6f6eee8090471ae01123e4ce009db5
EhWanu <cameronjshadgett@gmail.com>
2021-12-06 10:51:43 -0500

    search bu school or name

--- src/pages/clubs/components/all/Participating.jsx
+++ src/pages/clubs/components/all/Participating.jsx
@@ -1,32 +1,60 @@
 import "./participating.scss";
 import { studentClubs } from "../data";
+import { useState } from "react";
 
 const Participating = () => {
+	const [searchTerm, setSearchTerm] = useState("");
 	return (
 		<div className="participatingInfo">
 			<div className="container">
+				<input
+					type="text"
+					placeholder="find your club..."
+					className="search"
+					onChange={(event) => {
+						setSearchTerm(event.target.value);
+					}}
+				/>
 				<div className="cards">
-					{studentClubs.map((data, id) => (
-						<div className="card">
-							<a
-								href={data.link}
-								target="_blank"
-								rel="noopener noreferrer"
-								key={id}
-							>
-								<div className="infoBox">
-									<div className="textBox">
-										<h1>{data.name}</h1>
-										<h2>{data.school}</h2>
+					{studentClubs
+						.filter((val) => {
+							if (searchTerm === "") {
+								return val;
+							} else if (
+								val.name
+									.toLowerCase()
+									.includes(searchTerm.toLowerCase())
+							) {
+								return val;
+							} else if (
+								val.school
+									.toLowerCase()
+									.includes(searchTerm.toLowerCase())
+							) {
+								return val;
+							}
+						})
+						.map((data, id) => (
+							<div className="card">
+								<a
+									href={data.link}
+									target="_blank"
+									rel="noopener noreferrer"
+									key={id}
+								>
+									<div className="infoBox">
+										<div className="textBox">
+											<h1>{data.name}</h1>
+											<h2>{data.school}</h2>
+										</div>
 									</div>
-								</div>
-							</a>
+								</a>
 
-							<div className="imgContainer">
-								<img src={data.logo} alt="" />
+								<div className="imgContainer">
+									<img src={data.logo} alt="" />
+								</div>
 							</div>
-						</div>
-					))}
+						))}
 				</div>
 			</div>
 		</div>
