02477758dff056da4339e662a2bba4a9a76096a1
EhWanu <cameronjshadgett@gmail.com>
2021-12-07 13:46:36 -0500

    mobile for smaller screens

--- src/pages/get started page/components/implications/Implications.jsx
+++ src/pages/get started page/components/implications/Implications.jsx
@@ -1,83 +1,88 @@
 import "./implications.scss";
+import "../../../../global.scss";
 
 export default function Implications() {
 	return (
-		<div className="implications">
-			<div className="banner">
-				<h1>NTX IMPLICATION/RELATIONSHIP</h1>
+		<>
+			<div className="dividerBarBlue">
+				<h1 className="barText">
+					NTX IMPLICATION/RELATIONSHIP
+				</h1>
 			</div>
-			<div className="backgroundColumn"></div>
-			<div className="left">
-				<div className="imgContainer">
-					<img
-						src="https://images.unsplash.com/photo-1535957998253-26ae1ef29506?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=736&q=80"
-						alt=""
-					/>
+			<div className="implications">
+				<div className="backgroundColumn"></div>
+				<div className="left">
+					<div className="imgContainer">
+						<img
+							src="https://images.unsplash.com/photo-1535957998253-26ae1ef29506?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=736&q=80"
+							alt=""
+						/>
+					</div>
+				</div>
+				<div className="right">
+					<div className="textBox">
+						<p>
+							The student clubs are independant and have
+							their own administrative structure,
+							according to their university rules &
+							standards. NeuroTechX does not have any
+							governance nor authority over the club.
+							<br />
+							<br />
+							NeuroTechX is an international entity
+							putting a framework around neurotechnology
+							student clubs. Most universities won’t
+							finance a student club it they are the only
+							one alike on the planet. We help student
+							clubs point a finger at us showing that it
+							exists and more and more universities have
+							their neurotech student clubs and yours
+							should too.
+							<br />
+							<br />
+							NeuroTechX organizes one or more annual
+							competition, either online or physical in a
+							specific location and we strongly encourage
+							all the student clubs part of the network
+							to participate. You can find more
+							information about the competition here.
+							<br />
+							<br />
+							NeuroTechX acts as a partner/sponsor. And
+							should be displayed as such on your student
+							club marketing material (website, swag
+							merchandise, banners, etc.) You can find
+							NeuroTechX marketing assets here or in the
+							shared G-Drive folder.
+							<br />
+							<br />
+							NeuroTechX can help student clubs by
+							donating devices (e.g. Neurosky, Muse
+							Headband, OpenBCI Boards, etc.) this is at
+							the discretion of NeuroTechX Student Club
+							Committee.
+							<br />
+							<br />
+							NeuroTechX will feature your student club
+							on its website and give you as much
+							visibility as possible. (e.g. share your
+							events, your projects, etc.)
+							<br />
+							<br />
+							The student clubs should buy their devices
+							from NeuroTechX Store (work in progress).
+							The devices are the same price or less and
+							NeuroTechX gets money from it. 2 birds 1
+							stone.
+							<br />
+							<br />
+							We are working with our partners to get a
+							discount code for NeuroTechX Student Clubs
+							should be available soon.
+						</p>
+					</div>
 				</div>
 			</div>
-			<div className="right">
-				<div className="textBox">
-					<p>
-						The student clubs are independant and have
-						their own administrative structure,
-						according to their university rules &
-						standards. NeuroTechX does not have any
-						governance nor authority over the club.
-						<br />
-						<br />
-						NeuroTechX is an international entity
-						putting a framework around neurotechnology
-						student clubs. Most universities won’t
-						finance a student club it they are the only
-						one alike on the planet. We help student
-						clubs point a finger at us showing that it
-						exists and more and more universities have
-						their neurotech student clubs and yours
-						should too.
-						<br />
-						<br />
-						NeuroTechX organizes one or more annual
-						competition, either online or physical in a
-						specific location and we strongly encourage
-						all the student clubs part of the network to
-						participate. You can find more information
-						about the competition here.
-						<br />
-						<br />
-						NeuroTechX acts as a partner/sponsor. And
-						should be displayed as such on your student
-						club marketing material (website, swag
-						merchandise, banners, etc.) You can find
-						NeuroTechX marketing assets here or in the
-						shared G-Drive folder.
-						<br />
-						<br />
-						NeuroTechX can help student clubs by
-						donating devices (e.g. Neurosky, Muse
-						Headband, OpenBCI Boards, etc.) this is at
-						the discretion of NeuroTechX Student Club
-						Committee.
-						<br />
-						<br />
-						NeuroTechX will feature your student club on
-						its website and give you as much visibility
-						as possible. (e.g. share your events, your
-						projects, etc.)
-						<br />
-						<br />
-						The student clubs should buy their devices
-						from NeuroTechX Store (work in progress).
-						The devices are the same price or less and
-						NeuroTechX gets money from it. 2 birds 1
-						stone.
-						<br />
-						<br />
-						We are working with our partners to get a
-						discount code for NeuroTechX Student Clubs
-						should be available soon.
-					</p>
-				</div>
-			</div>
-		</div>
+		</>
 	);
 }
